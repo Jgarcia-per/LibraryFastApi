787ce949b7d3f0c3367aa904fa479a27a483392f
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-04 16:55:53 -0500

    Enhance book management: update update_book function for clarity and add delete_book endpoint

--- books.py
+++ books.py
@@ -43,7 +43,14 @@ async def create_book(new_book: dict = Body()):
 
 @app.put("/books/update_book/")
 async def update_book(update_book: dict = Body()):
-    for i, BOOKS in enumerate(BOOKS):
-        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold(): 
             BOOKS[i] = update_book
             return {'message': 'Book updated'}
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title:str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
