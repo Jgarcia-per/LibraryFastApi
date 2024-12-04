b241acbdc82debffccc030ecaae50b797eef5627
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-04 14:35:00 -0500

    Refactor API endpoints and update documentation; add book retrieval and management features

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -7,9 +7,43 @@ BOOKS = [
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
     {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
     {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
-    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Two', 'category': 'math'},
     ]
 
 @app.get("/books")
-async def first_api():
+async def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_book_tittle(book_tittle: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_tittle.casefold():
+            return book
+    return {'error': 'Book not found'}
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.post("/books/create_book/")
+async def create_book(new_book: dict = Body()):
+    BOOKS.append(new_book)
+
+@app.put("/books/update_book/")
+async def update_book(update_book: dict = Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+            BOOKS[i] = update_book
+            return {'message': 'Book updated'}
