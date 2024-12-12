c322a15ec84c14cd427fd2bd645c538547554023
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-12 16:53:09 -0500

    Implement database session management and add endpoint to retrieve all books

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 from models import BookModel
-from configs.Database import engine
+from configs.Database import engine, SessionLocal
 
 app = FastAPI()
 BookModel.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    """
+    Start DataBase
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    """
+    Get all Book
+    """
+    return db.query(BookModel.Book).all()
