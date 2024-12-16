bdda5022a5f03b4e04a3354afbb2f4d443f29607
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-16 16:55:41 -0500

    Refactor user creation endpoint to include database dependency and return status code; improve password hashing

--- routers/v1/UserRouter.py
+++ routers/v1/UserRouter.py
@@ -1,17 +1,21 @@
 from fastapi import APIRouter
+from starlette import status
+
 from models.UserRequestModel import UserRequest
 from models.UserModel import Users
 from passlib.context import CryptContext
+from configs.Database import db_dependency
 
 AuthRouter = APIRouter()
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@AuthRouter.post("/create")
-async def create_user(user_request: UserRequest):
+@AuthRouter.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, user_request: UserRequest):
     """
     Autenticate User
     """
-    create_user_model = Users(**user_request.dict())
-    create_user_model.password = bcrypt_context.hash(user_request.password)
+    user_model = Users(**user_request.dict())
+    user_model.password = bcrypt_context.hash(user_request.password)
 
-    return {create_user_model}
+    db.add(user_model)
+    db.commit()
