2176d16e6062a3148d0e87e9184f8f15380a550b
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-16 09:43:43 -0500

    Remove unnecessary whitespace in Database.py configuration

--- configs/Database.py
+++ configs/Database.py
@@ -6,9 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from fastapi import Depends
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./metadata/book.db'
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
