bd8311af65d8092f99f54003f96e0de836f9c93e
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-18 16:56:59 -0500

    Update database configuration to use environment variables and add required dependencies

--- configs/Database.py
+++ configs/Database.py
@@ -5,10 +5,11 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from configs.variables import USER_DB, PASSWORD_DB, HOST_DB, NAME_DB
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./metadata/library.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = f"postgresql://{USER_DB}:{PASSWORD_DB}@{HOST_DB}/{NAME_DB}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
