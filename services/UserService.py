7392dac8c37a035c7a2eae6c3ee9b22e8a25e731
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-18 14:21:54 -0500

    Refactor access token creation to improve parameter naming for clarity

--- services/UserService.py
+++ services/UserService.py
@@ -20,8 +20,8 @@ def authenticate_user (username: str, password: str, db):
         return False
     return user
 
-def create_acces_token(username: str, id: int, role: str):
-    encode = {"sub": username, "id": id, "role": role}
+def create_acces_token(username: str, user_id: int, role: str):
+    encode = {"sub": username, "id": user_id, "role": role}
     expires = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -30,12 +30,12 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
-        id: int = payload.get("id")
+        user_id: int = payload.get("id")
         role: str = payload.get("role")
         if username is None or role is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail="Could not validate user")
-        return{"Username": username, "id": id, "role": role}
+        return{"Username": username, "id": user_id, "role": role}
     except JWTError as exc:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Could not validate user") from exc
