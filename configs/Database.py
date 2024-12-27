4a55a3aabb3a642a9776d3754a6ec4d5c4ea8781
jgarcia-ASD <jgarcia@grupoasd.com>
2024-12-27 11:10:15 -0500

    Remove BookService.py and clean up Database.py by removing unnecessary whitespace

--- configs/Database.py
+++ configs/Database.py
@@ -6,7 +6,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from env import USER_DB, PASSWORD_DB, HOST_DB, NAME_DB
 
-
 SQLALCHEMY_DATABASE_URL = f"postgresql://{USER_DB}:{PASSWORD_DB}@{HOST_DB}/{NAME_DB}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
